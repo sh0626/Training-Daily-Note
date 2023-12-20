4ced53b68654f5d92ac1b1a083c718435b1866b3
sun <sonhokg@naver.com>
2023-12-20 09:48:33 +0900

    생성자

---
+++ Note/Code/12-19/JavaExam12_19_01/KoreanExampl.java
@@ -0,0 +1,11 @@
+package obj.JavaExam12_19_01;
+
+public class KoreanExampl {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("Park", "031-6497-6431");
+        System.out.println("k1.name : " + k1.name);
+        Korean k2 = new Korean("Sukk", "031-1585-2547");
+        System.out.println("k1.name : " + k2.name);
+
+    }
+}
